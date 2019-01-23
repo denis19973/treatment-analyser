ed807e7f956390acd26192e65c853e5f43514d24
denis19973 <oxingteam@gmail.com>
2019-01-23 11:56:24 +0700

    used indicoio for sentiment analysis in russian texts

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import tweepy
-from textblob import TextBlob
 import matplotlib.pyplot as plt
+import indicoio
 
-PROCESS_TWEETS_PER_CANDIDATE = 1500
+PROCESS_TWEETS_PER_CANDIDATE = 3000
+PROCESS_TEXT_PER_REQUEST = 100
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
+indicoio.config.api_key = '0f9c08f8865c31e46cc723e19f43e6dc'
 
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = 'x7z05c00lhUFx0ew1L02LPeWk'
+CONSUMER_SECRET = '05kt5WOfQUe0TKC2BlTpLV0uvBnqHKqYTZmvbYfhtBqXStppiS'
+
+ACCESS_TOKEN = '1087171855780179968-SVa21xDSjxF7j6vjHwewgCvzdC8fEy'
+ACCESS_TOKEN_SECRET = '1HkISUe4L2HMiW3BCJdWRcqWDrXcXIezpPzAsKRwIUee8'
 
 def search_tweets(query):
 	return tweepy.Cursor(api.search,
@@ -16,27 +19,37 @@ def search_tweets(query):
                            count=100,
                            result_type="recent",
                            include_entities=False,
-                           lang="en").items()
+                           lang="ru").items()
 
-def analyse_text_sentiment(text):
-	text_blob = TextBlob(text)
-	return text_blob.sentiment.polarity
+def analyse_text_sentiment(texts: list):
+	sentiments = indicoio.sentiment(texts, language='detect')
+	return [s for s in sentiments if s is not None]
 
 def plot_sentiment(text, sentiment_value):
     plt.scatter(text, sentiment_value, color='black')
 
 def process_and_plot(text):
     processed = 0
-    value = 0
+    sentiment_value = 0
+    tweets_chunk = []
     for tweet in search_tweets(text):
-        processed += 1
-        sentiment = analyse_text_sentiment(tweet.text)
-        value += sentiment
+        tweets_chunk.append(tweet.text)
+        if len(tweets_chunk) == PROCESS_TEXT_PER_REQUEST:
+            sentiments = analyse_text_sentiment(tweets_chunk)
+            sentiment_value += sum(sentiments)
+            processed += len(sentiments)
+            tweets_chunk = []
         if processed >= PROCESS_TWEETS_PER_CANDIDATE:
-        	break
-    plot_sentiment(text, value/processed)
+          break
+    if tweets_chunk:
+        sentiments = analyse_text_sentiment(tweets_chunk)
+        sentiment_value += sum(sentiments)
+        processed += len(sentiments)
+    if processed:
+      sentiment = sentiment_value / processed
+      plot_sentiment(text, sentiment)
 
-plt.ylabel('Sentiment')
+plt.ylabel('Sentiment(0 - negative, 1 - positive)')
 plt.title('Treatment to Candidates')
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -44,9 +57,9 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-process_and_plot('Zelenskiy')
-process_and_plot('Timoshenko')
-process_and_plot('Poroshenko')
+process_and_plot('Зеленский')
+process_and_plot('Тимошенко')
+process_and_plot('Порошенко')
 
 plt.legend()
 plt.show()
